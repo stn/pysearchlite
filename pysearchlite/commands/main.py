1cf257423cd38a7eb84a8eebc56c3121bc2c0d78
Akira Ishino <akira.ishino@gmail.com>
2022-07-19 00:06:40 +0900

    Move main.py into commands sub-package

---
+++ pysearchlite/commands/main.py
@@ -0,0 +1,31 @@
+import json
+import sys
+
+import pysearchlite as psl
+
+
+INDEX_DIR = 'idx'
+
+
+def index_file(docs_dir: str):
+    filename = sys.argv[1]
+    with open(filename, 'r', encoding='utf-8') as f:
+        text = ' '.join(psl.normalized_tokens(f.read()))
+        psl.index(filename, text)
+
+
+def main():
+    # Index
+    for line in sys.stdin:
+        d = json.loads(line)
+        psl.index(d['id'], d['text'])
+    psl.save_index(INDEX_DIR)
+    psl.restore_index(INDEX_DIR)
+    # Search
+    print(psl.search('the'))
+    print(psl.search('los angeles'))
+    print(psl.search('the national football league'))
+
+
+if __name__ == '__main__':
+    main()
