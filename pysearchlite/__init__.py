d010962397a875d7f25ae8b53e37008ed4a41642
Akira Ishino <akira.ishino@gmail.com>
2022-07-19 00:28:30 +0900

    v0.2.1
    - specify importing functions explicitly

--- pysearchlite/__init__.py
+++ pysearchlite/__init__.py
@@ -1,4 +1,10 @@
-from .search_engine import *
+from .search_engine import (
+    index,
+    normalized_tokens,
+    restore_index,
+    save_index,
+    search,
+)
 
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
