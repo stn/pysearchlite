fd1dcf439928beb9985a9487a22cb7ce4186c0a0
Akira Ishino <akira.ishino@gmail.com>
2022-07-24 16:35:30 +0900

    Change byteorder to "big"

--- pysearchlite/inverted_index.py
+++ pysearchlite/inverted_index.py
@@ -4,11 +4,11 @@ import os
 import shutil
 import tempfile
 from operator import itemgetter
-from sys import byteorder
 from typing import Optional, TextIO, BinaryIO, Union
 
 INVERTED_INDEX_FILENAME = "inverted_index"
 
+BYTEORDER = "big"
 TOKEN_LEN_BYTES = 2
 DOCID_BYTES = 4
 DOCID_LEN_BYTES = 4
@@ -122,20 +122,20 @@ class SinglePassInMemoryInvertedIndex(InvertedIndex):
 
     def write_token(self, f: BinaryIO, token: str):
         encoded_token = token.encode('utf-8')
-        f.write(len(encoded_token).to_bytes(TOKEN_LEN_BYTES, byteorder))
+        f.write(len(encoded_token).to_bytes(TOKEN_LEN_BYTES, BYTEORDER))
         f.write(encoded_token)
 
     def read_token(self, f: Union[BinaryIO, mmap.mmap]) -> str:
         token_bytes = f.read(TOKEN_LEN_BYTES)
         if not token_bytes:
             return ""
-        token_len = int.from_bytes(token_bytes, byteorder)
+        token_len = int.from_bytes(token_bytes, BYTEORDER)
         return f.read(token_len).decode('utf-8')
 
     def write_doc_ids(self, f: BinaryIO, doc_ids: list[int]):
-        f.write(len(doc_ids).to_bytes(DOCID_LEN_BYTES, byteorder))
+        f.write(len(doc_ids).to_bytes(DOCID_LEN_BYTES, BYTEORDER))
         for doc_id in doc_ids:
-            f.write(doc_id.to_bytes(DOCID_BYTES, byteorder))
+            f.write(doc_id.to_bytes(DOCID_BYTES, BYTEORDER))
 
     def save_raw_data(self):
         # [len(token)] [token] [len(ids)] [ids]
@@ -151,15 +151,15 @@ class SinglePassInMemoryInvertedIndex(InvertedIndex):
     def merge_index(self, idx1: str, idx2: str):
         def copy_ids(dst: BinaryIO, src: BinaryIO):
             docid_bytes = src.read(DOCID_LEN_BYTES)
-            doc_ids_len = int.from_bytes(docid_bytes, byteorder)
+            doc_ids_len = int.from_bytes(docid_bytes, BYTEORDER)
             dst.write(docid_bytes)
             doc_ids_bytes = src.read(doc_ids_len * DOCID_BYTES)
             dst.write(doc_ids_bytes)
 
         def merge_ids(dst: BinaryIO, src1: BinaryIO, src2: BinaryIO):
-            doc_ids_len1 = int.from_bytes(src1.read(DOCID_LEN_BYTES), byteorder)
-            doc_ids_len2 = int.from_bytes(src2.read(DOCID_LEN_BYTES), byteorder)
-            dst.write((doc_ids_len1 + doc_ids_len2).to_bytes(DOCID_LEN_BYTES, byteorder))
+            doc_ids_len1 = int.from_bytes(src1.read(DOCID_LEN_BYTES), BYTEORDER)
+            doc_ids_len2 = int.from_bytes(src2.read(DOCID_LEN_BYTES), BYTEORDER)
+            dst.write((doc_ids_len1 + doc_ids_len2).to_bytes(DOCID_LEN_BYTES, BYTEORDER))
             doc_ids_bytes = src1.read(doc_ids_len1 * DOCID_BYTES)
             dst.write(doc_ids_bytes)
             doc_ids_bytes = src2.read(doc_ids_len2 * DOCID_BYTES)
@@ -229,7 +229,7 @@ class SinglePassInMemoryInvertedIndex(InvertedIndex):
         while token:
             pos = self.mmap.tell()
             self.data[token] = pos
-            ids_len = int.from_bytes(self.mmap.read(DOCID_LEN_BYTES), byteorder)
+            ids_len = int.from_bytes(self.mmap.read(DOCID_LEN_BYTES), BYTEORDER)
             self.mmap.seek(ids_len * DOCID_BYTES, 1)
             token = self.read_token(self.mmap)
 
@@ -237,17 +237,17 @@ class SinglePassInMemoryInvertedIndex(InvertedIndex):
         pos = self.data.get(token, -1)
         if pos < 0:
             return []
-        ids_len = int.from_bytes(self.mmap[pos:pos+DOCID_LEN_BYTES], byteorder)
+        ids_len = int.from_bytes(self.mmap[pos:pos+DOCID_LEN_BYTES], BYTEORDER)
         pos += DOCID_LEN_BYTES
         ids = []
         for _ in range(ids_len):
-            ids.append(int.from_bytes(self.mmap[pos:pos+DOCID_BYTES], byteorder))
+            ids.append(int.from_bytes(self.mmap[pos:pos+DOCID_BYTES], BYTEORDER))
             pos += DOCID_BYTES
         return ids
 
     def next_doc_id(self, pos):
         npos = pos + DOCID_BYTES
-        doc_id = int.from_bytes(self.mmap[pos:npos], byteorder)
+        doc_id = int.from_bytes(self.mmap[pos:npos], BYTEORDER)
         return doc_id, npos
 
     def search_and(self, tokens: list[str]) -> list[int]:
@@ -267,9 +267,9 @@ class SinglePassInMemoryInvertedIndex(InvertedIndex):
         max_doc_id = -1
         for pos in file_pos:
             npos = pos + DOCID_LEN_BYTES
-            doc_ids_len = int.from_bytes(self.mmap[pos:npos], byteorder)
+            doc_ids_len = int.from_bytes(self.mmap[pos:npos], BYTEORDER)
             npos2 = npos + DOCID_BYTES
-            doc_id = int.from_bytes(self.mmap[npos:npos2], byteorder)
+            doc_id = int.from_bytes(self.mmap[npos:npos2], BYTEORDER)
             state.append((doc_ids_len - 1, doc_id, npos2))
             if doc_id > max_doc_id:
                 max_doc_id = doc_id
--- pysearchlite/test_inverted_index.py
+++ pysearchlite/test_inverted_index.py
@@ -8,6 +8,17 @@ from .inverted_index import (
     SinglePassInMemoryInvertedIndex,
 )
 
+# Big endian
+B_INT16_1 = b'\x00\x01'
+B_INT16_5 = b'\x00\x05'
+
+B_INT32_0 = b'\x00\x00\x00\x00'
+B_INT32_1 = b'\x00\x00\x00\x01'
+B_INT32_2 = b'\x00\x00\x00\x02'
+B_INT32_3 = b'\x00\x00\x00\x03'
+B_INT32_4 = b'\x00\x00\x00\x04'
+B_INT32_5 = b'\x00\x00\x00\x05'
+
 
 @pytest.fixture
 def idx_dir() -> str:
@@ -38,11 +49,11 @@ def test_spim_tmp_index_name(spim_index):
 def test_spim_write_token(spim_index):
     f = io.BytesIO()
     spim_index.write_token(f, 'hello')
-    assert f.getvalue() == b'\x05\x00hello'
+    assert f.getvalue() == B_INT16_5 + b'hello'
 
 
 def test_spim_read_token(spim_index):
-    f = io.BytesIO(b'\x05\x00world')
+    f = io.BytesIO(B_INT16_5 + b'world')
     token = spim_index.read_token(f)
     assert token == 'world'
 
@@ -50,7 +61,7 @@ def test_spim_read_token(spim_index):
 def test_write_doc_ids(spim_index):
     f = io.BytesIO()
     spim_index.write_doc_ids(f, [1, 2, 5])
-    assert f.getvalue() == b'\x03\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x05\x00\x00\x00'
+    assert f.getvalue() == B_INT32_3 + B_INT32_1 + B_INT32_2 + B_INT32_5
 
 
 def test_save_raw_data(spim_index):
@@ -59,9 +70,9 @@ def test_save_raw_data(spim_index):
     spim_index.save_raw_data()
     with open(spim_index.tmp_index_name(0), 'rb') as f:
         tmp_index = f.read()
-        assert tmp_index == b'\x01\x00a\x01\x00\x00\x00\x01\x00\x00\x00' \
-                            b'\x01\x00b\x01\x00\x00\x00\x00\x00\x00\x00' \
-                            b'\x01\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'
+        assert tmp_index == (B_INT16_1 + b'a' + B_INT32_1 + B_INT32_1 +
+                             B_INT16_1 + b'b' + B_INT32_1 + B_INT32_0 +
+                             B_INT16_1 + b'c' + B_INT32_2 + B_INT32_0 + B_INT32_1)
     assert spim_index.raw_data == {}
     assert spim_index.raw_data_size == 0
     assert spim_index.tmp_index_num == 1
@@ -73,25 +84,25 @@ def test_spim_save(spim_index):
     spim_index.save()
     with open(os.path.join(spim_index.idx_dir, 'inverted_index'), 'rb') as f:
         tmp_index = f.read()
-        assert tmp_index == b'\x01\x00a\x01\x00\x00\x00\x01\x00\x00\x00' \
-                            b'\x01\x00b\x01\x00\x00\x00\x00\x00\x00\x00' \
-                            b'\x01\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'
+        assert tmp_index == (B_INT16_1 + b'a' + B_INT32_1 + B_INT32_1 +
+                             B_INT16_1 + b'b' + B_INT32_1 + B_INT32_0 +
+                             B_INT16_1 + b'c' + B_INT32_2 + B_INT32_0 + B_INT32_1)
 
 
 def test_spim_restore(spim_index):
     with open(os.path.join(spim_index.idx_dir, 'inverted_index'), 'wb') as f:
-        f.write(b'\x01\x00a\x01\x00\x00\x00\x01\x00\x00\x00'
-                b'\x01\x00b\x01\x00\x00\x00\x00\x00\x00\x00'
-                b'\x01\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00')
+        f.write(B_INT16_1 + b'a' + B_INT32_1 + B_INT32_1 +
+                B_INT16_1 + b'b' + B_INT32_1 + B_INT32_0 +
+                B_INT16_1 + b'c' + B_INT32_2 + B_INT32_0 + B_INT32_1)
     spim_index.restore()
     assert spim_index.data == {'a': 2+1, 'b': (2+1+4+4)+2+1, 'c': (2+1+4+4)*2+2+1}
 
 
 def test_spim_get(spim_index):
     with open(os.path.join(spim_index.idx_dir, 'inverted_index'), 'wb') as f:
-        f.write(b'\x01\x00a\x01\x00\x00\x00\x01\x00\x00\x00'
-                b'\x01\x00b\x01\x00\x00\x00\x00\x00\x00\x00'
-                b'\x01\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00')
+        f.write(B_INT16_1 + b'a' + B_INT32_1 + B_INT32_1 +
+                B_INT16_1 + b'b' + B_INT32_1 + B_INT32_0 +
+                B_INT16_1 + b'c' + B_INT32_2 + B_INT32_0 + B_INT32_1)
     spim_index.restore()
     assert spim_index.get('a') == [1]
     assert spim_index.get('b') == [0]
@@ -101,9 +112,9 @@ def test_spim_get(spim_index):
 
 def test_spim_search_and(spim_index):
     with open(os.path.join(spim_index.idx_dir, 'inverted_index'), 'wb') as f:
-        f.write(b'\x01\x00a\x01\x00\x00\x00\x01\x00\x00\x00'
-                b'\x01\x00b\x01\x00\x00\x00\x00\x00\x00\x00'
-                b'\x01\x00c\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00')
+        f.write(B_INT16_1 + b'a' + B_INT32_1 + B_INT32_1 +
+                B_INT16_1 + b'b' + B_INT32_1 + B_INT32_0 +
+                B_INT16_1 + b'c' + B_INT32_2 + B_INT32_0 + B_INT32_1)
     spim_index.restore()
     assert spim_index.search_and(['a', 'b']) == []
     assert spim_index.search_and(['a', 'c']) == [1]
