5cab478a42b103c26c5b0355eb5873c9d2764c58
Akira Ishino <akira.ishino@gmail.com>
2022-07-30 23:26:57 +0900

    Use SinglePassInMemoryInvertedIndex

--- pysearchlite/search_engine.py
+++ pysearchlite/search_engine.py
@@ -3,9 +3,9 @@ from typing import Optional
 from .doc_list import DocList, MemoryDocList
 from .inverted_index import InvertedIndex
 # from .memory_inverted_index import MemoryInvertedIndex
-# from .spim_inveted_index import SinglePassInMemoryInvertedIndex
+from .spim_inveted_index import SinglePassInMemoryInvertedIndex
 # from .spim_inveted_index_memory import SinglePassInMemoryInvertedIndexMemory
-from .spim_inveted_index_memory_binary import SinglePassInMemoryInvertedIndexMemoryBinary
+# from .spim_inveted_index_memory_binary import SinglePassInMemoryInvertedIndexMemoryBinary
 from .tokenize import normalized_tokens
 
 
@@ -16,7 +16,7 @@ INVERTED_INDEX: Optional[InvertedIndex] = None
 def init(idx_dir: str):
     global DOC_LIST, INVERTED_INDEX
     DOC_LIST = MemoryDocList(idx_dir)
-    INVERTED_INDEX = SinglePassInMemoryInvertedIndexMemoryBinary(idx_dir)
+    INVERTED_INDEX = SinglePassInMemoryInvertedIndex(idx_dir)
 
 
 def index(name: str, text: str):
