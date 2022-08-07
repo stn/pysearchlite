b8d02b9fd693c330b72ae2d87ef097f5eba19a2e
Akira Ishino <akira.ishino@gmail.com>
2022-08-07 13:42:23 +0900

    Split ids from skip_lists

--- pysearchlite/block_skip_list.py
+++ pysearchlite/block_skip_list.py
@@ -10,6 +10,7 @@ OFFSET = int(os.environ.get('PYSEARCHLITE_SKIPLIST_OFFSET', '5'))
 class BlockSkipList(object):
 
     def __init__(self):
+        self.ids = None
         self.p = SKIPLIST_P
         self.skip_lists = None
         self.current_pos = None
@@ -18,7 +19,7 @@ class BlockSkipList(object):
     @staticmethod
     def from_list(ids: list[int], p=SKIPLIST_P, offset=OFFSET):
         max_height = math.log(max(len(ids) - 1, 1), p) - offset
-        skip_lists = [ids]
+        skip_lists = [[]]
         for i in range(1, len(ids)):
             level = 1
             m = p
@@ -29,33 +30,33 @@ class BlockSkipList(object):
                 level += 1
                 m *= p
         s = BlockSkipList()
+        s.ids = array('i', ids)
         s.p = p
         s.skip_lists = [array('i', sl) for sl in skip_lists]
         return s
 
     def get_ids(self) -> list[int]:
-        return self.skip_lists[0].tolist()
+        return self.ids.tolist()
 
     def search(self, doc_id_a: int):
         if len(self.skip_lists) == 1:
             # no skip list
-            ids = self.skip_lists[0]
-            for i in range(self.current_pos[0], len(ids)):
-                if ids[i] >= doc_id_a:
+            for i in range(self.current_pos[0], len(self.ids)):
+                if self.ids[i] >= doc_id_a:
                     self.current_pos[0] = i
-                    return ids[i]
-            return ids[-1]
+                    return self.ids[i]
+            return self.ids[-1]
 
         # Check the start position.
         for level in range(0, len(self.skip_lists)):
             if doc_id_a <= self.last_id[level]:
                 break
         pos = self.current_pos[level]
-        skip_list = self.skip_lists[level]
-
-        # When the first item is greater than the given doc id.
-        if skip_list[pos] >= doc_id_a:
-            return skip_list[pos]
+        if level > 0:
+            skip_list = self.skip_lists[level]
+            # When the first item is greater than the given doc id.
+            if skip_list[pos] >= doc_id_a:
+                return skip_list[pos]
 
         # skip list
         while level > 0:
@@ -82,20 +83,20 @@ class BlockSkipList(object):
             skip_list = self.skip_lists[level]
 
         # ids
-        for i in range(pos, len(skip_list)):
-            pos_id = skip_list[i]
+        for i in range(pos, len(self.ids)):
+            pos_id = self.ids[i]
             if pos_id >= doc_id_a:
                 self.current_pos[0] = i
                 self.last_id[0] = pos_id
                 return pos_id
-        self.current_pos[0] = len(skip_list) - 1
-        self.last_id[0] = skip_list[-1]
-        return skip_list[-1]
+        self.current_pos[0] = len(self.ids) - 1
+        self.last_id[0] = self.ids[-1]
+        return self.ids[-1]
 
     def intersection(self, b) -> array:
         b.reset()
         result = array('i')
-        for doc_id_a in self.skip_lists[0]:
+        for doc_id_a in self.ids:
             doc_id_b = b.search(doc_id_a)
             if doc_id_b == doc_id_a:
                 result.append(doc_id_a)
