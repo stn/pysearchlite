4a68b14d3714c8f06277848d8c32b92b131f72e3
Akira Ishino <akira.ishino@gmail.com>
2022-08-08 21:09:38 +0900

    Add DocIdList and SingleDocId

--- pysearchlite/block_skip_list.py
+++ pysearchlite/block_skip_list.py
@@ -18,7 +18,13 @@ class BlockSkipList(object):
 
     @staticmethod
     def from_list(ids: list[int], p=SKIPLIST_P, max_level=SKIPLIST_MAX_LEVEL):
+        if len(ids) == 1:
+            return SingleDocId(ids[0])
+        elif len(ids) <= p:
+            return DocIdList(ids)
+
         max_height = min(math.log(max(len(ids) - 1, 1), p), max_level)
+
         skip_lists = [[]]
         for i in range(1, len(ids)):
             level = 1
@@ -114,3 +120,76 @@ class BlockSkipList(object):
     def reset(self):
         self.current_pos = [0] * len(self.skip_lists)
         self.last_id = [-1] * len(self.skip_lists)
+
+
+class DocIdList(object):
+
+    def __init__(self, ids):
+        self.ids = array('i', ids)
+        self.current_pos = 0
+
+    def get_ids(self) -> list[int]:
+        return self.ids.tolist()
+
+    def search(self, doc_id_a: int):
+        for i in range(self.current_pos, len(self.ids)):
+            if self.ids[i] >= doc_id_a:
+                self.current_pos = i
+                return self.ids[i]
+        return self.ids[-1]
+
+    def intersection(self, b) -> array:
+        b.reset()
+        result = array('i')
+        # assume a < b
+        for doc_id_a in self.ids:
+            doc_id_b = b.search(doc_id_a)
+            if doc_id_b == doc_id_a:
+                result.append(doc_id_a)
+        return result
+
+    def intersection_with_doc_ids(self, a: array) -> array:
+        self.reset()
+        result = array('i')
+        # assume a < b
+        for doc_id_a in a:
+            doc_id_b = self.search(doc_id_a)
+            if doc_id_b == doc_id_a:
+                result.append(doc_id_a)
+        return result
+
+    def reset(self):
+        self.current_pos = 0
+
+
+class SingleDocId(object):
+
+    def __init__(self, doc_id):
+        self.doc_id = doc_id
+
+    def get_ids(self) -> list[int]:
+        return [self.doc_id]
+
+    def search(self, doc_id_a: int):
+        # it can return a smaller id than id_a.
+        return self.doc_id
+
+    def intersection(self, b) -> array:
+        # assume a < b
+        b.reset()
+        doc_id_b = b.search(self.doc_id)
+        result = array('i')
+        if doc_id_b == self.doc_id:
+            result.append(doc_id_b)
+        return result
+
+    def intersection_with_doc_ids(self, a: array) -> array:
+        result = array('i')
+        # assume a < this. so len(a) should be 1.
+        for doc_id_a in a:
+            if self.doc_id == doc_id_a:
+                result.append(doc_id_a)
+        return result
+
+    def reset(self):
+        pass
