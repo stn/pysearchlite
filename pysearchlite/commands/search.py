91a5a0f7963707a9e35ef429adf9e59e22d8e621
Akira Ishino <akira.ishino@gmail.com>
2022-09-03 22:36:19 +0900

    Update main and add search

--- pysearchlite/commands/search.py
+++ pysearchlite/commands/search.py
@@ -6,15 +6,9 @@ import pysearchlite as psl
 
 def main():
     psl.init('idx')
-    # Index
-    for line in sys.stdin:
-        doc = json.loads(line)
-        psl.index(doc['id'], doc['text'])
-    psl.save_index()
-    psl.clear_index()
     psl.restore_index()
     # Search
-    print(psl.search('st petersburg high school'))
+    print(psl.count('st petersburg high school'))
     print(psl.search('united states constitution'))
     print(psl.search('search'))
     print(psl.search('los angeles'))
