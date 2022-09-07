daf0866553baeb2f9023ff17ba3b7e58be941a32
Akira Ishino <akira.ishino@gmail.com>
2022-09-07 22:24:28 +0900

    Add bytes_gamma as C function

---
+++ pysearchlite/gamma_codecs_cffi_builder.py
@@ -0,0 +1,26 @@
+from cffi import FFI
+ffibuilder = FFI()
+
+
+ffibuilder.cdef("""
+int bytes_gamma(const unsigned char * mem, size_t pos);
+""")
+
+
+ffibuilder.set_source("pysearchlite._gamma_codecs_cffi", """
+unsigned int VAR_ENCODE_BYTES[] = {
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+5, 5, 5, 5, 5, 5, 5, 5
+};
+
+int bytes_gamma(const unsigned char * mem, size_t pos) {
+  return VAR_ENCODE_BYTES[(unsigned int)(mem[pos])];
+}
+""")
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
