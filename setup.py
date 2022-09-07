c40ffec1f9491076f12ed61fa2db5d20ffa62848
Akira Ishino <akira.ishino@gmail.com>
2022-09-07 22:41:35 +0900

    Update setup.py for CFFI

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
 
-setup()
+setup(
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["pysearchlite/gamma_codecs_cffi_builder.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
+)
